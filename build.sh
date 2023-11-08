7c849e3cbb0fb707c4c985c033efd42e182f4423
trifolium.wang <trifolium.wang@foxmail.com>
2023-11-08 09:58:56 +0800

    修改build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-exclude_list=".gitignore .out .build.sh .LICENSE README.md .git .idea"
+exclude_list=".gitignore out build.sh LICENSE README.md .git .idea"
+rm -rf ./out
 mkdir -p ./out
 for file in *
 do
     if [[ ! " $exclude_list " =~ " $file " ]]; then
-        cp "$file" ./out
+        cp -r "$file" ./out
     fi
 done
 echo "build success"
